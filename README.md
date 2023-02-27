4bdebf042ed72c31ac886d4642b9f205e151cd34
Alex <ax.dev@yahoo.com>
2023-02-27 17:21:44 +0300

    try gradiet back

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 ## Title Text
 
+Just for fun!
+
 RGB https://www.rapidtables.com/web/color/RGB_Color.html
 
 ---
 
-Скрипт работает на базе [Pillow](https://pypi.org/project/Pillow/), он создаёт изображение с текстом посередине. Цвет шрифт/фон, размер текста, шрифт можно менять в скрипте.
+`title_text.py` Скрипт работает на базе [Pillow](https://pypi.org/project/Pillow/), он создаёт изображение с текстом посередине. Цвет шрифт/фон, размер текста, шрифт можно менять в скрипте.
+
+---
+
+`text_gen2.py` Содаёт текст из бекграунда.
---
+++ text_gen2.py
@@ -0,0 +1,30 @@
+from PIL import Image, ImageDraw, ImageFont
+
+BACKGROUND_IMAGE_FILENAME = 'cookie_cutter_background_cropped.png'
+RESULT_IMAGE_FILENAME = 'cookie_cutter_text_result.png'
+THE_TEXT = 'AX2048'
+FONT_NAME = 'Archangelsk.ttf'  # Arial Bold
+
+# Read the background image and convert to an RGB image with Alpha.
+with open(BACKGROUND_IMAGE_FILENAME, 'rb') as file:
+    bgr_img = Image.open(file)
+    bgr_img = bgr_img.convert('RGBA')  # Give iamge an alpha channel.
+    bgr_img_width, bgr_img_height = bgr_img.size
+    cx, cy = bgr_img_width//2, bgr_img_height//2  # Center of image.
+
+# Create a transparent foreground to be result of non-text areas.
+fgr_img = Image.new('RGBA', bgr_img.size, color=(13, 17, 23)) # (13, 17, 23) (0, 0, 0, 0)
+
+font_size = bgr_img_width//len(THE_TEXT)
+font = ImageFont.truetype(FONT_NAME, font_size)
+
+txt_width, txt_height = font.getsize(THE_TEXT)  # Size of text w/font if rendered.
+tx, ty = cx - txt_width//2, cy - txt_height//2  # Center of text.
+
+mask_img = Image.new('L', bgr_img.size, color=255)
+mask_img_draw = ImageDraw.Draw(mask_img)
+mask_img_draw.text((tx, ty), THE_TEXT, fill=0, font=font, align='center')
+
+res_img = Image.composite(fgr_img, bgr_img, mask_img)
+res_img.save(RESULT_IMAGE_FILENAME)
+#res_img.show()
