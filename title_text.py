c37698110ba469058e71af2fc6a333a0166261b6
Alex <ax.dev@yahoo.com>
2023-02-27 16:40:07 +0300

    github colors

--- title_text.py
+++ title_text.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 
-img = Image.new('RGB', (1366, 768), color = 'black')
+img = Image.new('RGB', (1366, 768), color = (13, 17, 23))
 
 fnt = ImageFont.truetype('Archangelsk.ttf', 100)
 text = 'Variable Text'
@@ -11,6 +11,6 @@ x = (1366 - textwidth) / 2
 y = (768 - textheight) / 2
 
 gradient = [(0, 0, 255), (255, 0, 255)]
-d.text((x, y), text, font=fnt, fill=(200, 111, 200))
+d.text((x, y), text, font=fnt, fill=(201, 209, 217))
 
 img.save('image.png')
